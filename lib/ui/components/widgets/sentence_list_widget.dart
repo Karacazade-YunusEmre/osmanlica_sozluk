d3781e2260e605e514a3383fe3360ce4647db29e
yeyemre <yeymre@gmail.com>
2022-09-28 20:46:55 +0300

    # uygulama'ya animation list özelliği eklendi

--- lib/ui/components/widgets/sentence_list_widget.dart
+++ lib/ui/components/widgets/sentence_list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 
 import '../../../controller/main_controller.dart';
@@ -36,18 +37,29 @@ class _SentenceListWidgetState extends State<SentenceListWidget> {
                   color: Colors.deepOrangeAccent,
                 ),
               )
-            : ListView.builder(
-                itemCount: mainController.sentenceList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  SentenceModel currentSentence = mainController.sentenceList[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SentenceItemWidget(
-                      currentSentence: currentSentence,
-                    ),
-                  );
-                },
-              ),
+            : AnimationLimiter(
+              child: ListView.builder(
+                  itemCount: mainController.sentenceList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    SentenceModel currentSentence = mainController.sentenceList[index];
+                    return AnimationConfiguration.staggeredList(
+                      position: index,
+                      duration: const Duration(milliseconds: 500),
+                      child: SlideAnimation(
+                        verticalOffset: 50.0,
+                        child: FadeInAnimation(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SentenceItemWidget(
+                              currentSentence: currentSentence,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+            ),
       ),
     );
   }
